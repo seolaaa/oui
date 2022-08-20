354f9c0278df81a762220969b38dc51b543cb9f2
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-08-20 18:15:10 +0900

    commit
    commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+
+window.addEventListener('load',()=>{
+
+    const content = document.querySelectorAll('.scroll-wrap')
+    const text1 = document.querySelector('#title-1')
+    const text2 = document.querySelector('#title-2')
+    const text3 = document.querySelector('#title-in-text')
+
+    const windowHeight = window.innerHeight;
+
+    let index = 0;
+    let isWheel = false;
+
+   
+    initEvent();
+
+   
+
+    function initEvent(){
+        window.addEventListener('scroll',scrollWindow)
+    }
+
+ 
+
+    function scrollWindow(){
+
+        let scrollHeight = window.pageYOffset;
+
+        if(scrollHeight>=900){
+            gsap.to(text1,{top:200,opacity:1,duration:0.5,ease:'power1.out',delay:0})
+            gsap.to(text2,{top:320,opacity:1,duration:0.5,ease:'power1.out',delay:0.2})
+            gsap.to(text3,{top:600,opacity:1,duration:0.5,ease:'power1.out',delay:0.4}),isWheel=false;
+        }
+        
+    }
+
+            
+        
+            
+    
+
+
+    
+
+      
+})
