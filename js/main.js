c74b07a07d6bc6481b4d27ad724d08367afc72d9
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-08-22 18:43:54 +0900

    8/22 수정

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('load',()=>{
 
     const content = document.querySelectorAll('.scroll-wrap')
     const text1 = document.querySelector('#title-1')
-    const text2 = document.querySelector('#title-2')
+    const text2 = document.querySelector('.on-text')
     const text3 = document.querySelector('#title-in-text')
 
     const windowHeight = window.innerHeight;
@@ -29,8 +29,8 @@ window.addEventListener('load',()=>{
 
         if(scrollHeight>=900){
             gsap.to(text1,{top:220,opacity:1,duration:0.5,ease:'power1.out',delay:0})
-            gsap.to(text2,{top:340,opacity:1,duration:0.5,ease:'power1.out',delay:0.2})
-            gsap.to(text3,{top:580,opacity:1,duration:0.5,ease:'power1.out',delay:0.4}),isWheel=false;
+            gsap.to(text2,{top:220,opacity:1,duration:0.5,ease:'power1.out',delay:0.2})
+            gsap.to(text3,{top:680,opacity:1,duration:0.5,ease:'power1.out',delay:0.4}),isWheel=false;
         }
         
     }
