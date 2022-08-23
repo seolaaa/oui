a5b53ddb17b047c1110bc04347e5bf6523066fee
seolaaa <109854037+seolaaa@users.noreply.github.com>
2022-08-23 10:33:55 +0900

    main js 수정

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@
 window.addEventListener('load',()=>{
 
     const content = document.querySelectorAll('.scroll-wrap')
-    const text1 = document.querySelector('#title-1')
+    const text1 = document.querySelector('.title-1')
     const text2 = document.querySelector('.on-text')
     const text3 = document.querySelector('#title-in-text')
+    const img = document.querySelector('#main-image')
 
     const windowHeight = window.innerHeight;
 
@@ -27,10 +28,14 @@ window.addEventListener('load',()=>{
 
         let scrollHeight = window.pageYOffset;
 
-        if(scrollHeight>=800){
-            // gsap.to(text1,{top:220,opacity:1,duration:0.5,ease:'power1.out',delay:0})
-            gsap.to(text2,{width:864,opacity:1,duration:0.5,ease:'power1.out',delay:0})
-            gsap.to(text3,{top:680,opacity:1,duration:0.5,ease:'power1.out',delay:0.5}),isWheel=false;
+        if(scrollHeight>=700){
+            gsap.to(text1,{opacity:1,duration:0.5,ease:'power1.out',delay:0})
+        }
+
+        if(scrollHeight>=900){
+            gsap.to(text2,{width:864,opacity:1,duration:1,ease:'power1.out',delay:0.5})
+            gsap.to(text3,{top:680,opacity:1,duration:0.5,ease:'power1.out',delay:0.7})
+            gsap.to(img,{opacity:1,duration:0.5,ease:'power1.out',delay:0.2}),isWheel=false;
         }
         
     }
